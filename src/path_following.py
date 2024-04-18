b66bbd090abe15fc410c884fa17e2d483a04926c
TamVanum1 <freder2001@gmail.com>
2024-04-18 16:34:40 -0400

    increase speed

--- src/path_following.py
+++ src/path_following.py
@@ -95,7 +95,7 @@ class PathNode(Node):
         filtered_path = helper.filter_path(self.result_path)
         self.curve = helper.bezier_curve(filtered_path)    
     
-        self.dt = 1
+        self.dt = .5
         self.t = self.dt
         # for PID control
         self.kp = 1
